2f4095959b39653c791e5a8d440bf62602681ca9
Julian Easterling <julian@julianscorner.com>
2012-09-23 21:59:19 -0400

    updated "CALL" to "call"

--- git-svn-commit-project.bat
+++ git-svn-commit-project.bat
@@ -2,32 +2,32 @@
 
 setlocal
 
-CALL C:\bin\development-tools\_dev_settings.cmd
+call C:\bin\development-tools\_dev_settings.cmd
 call C:\bin\development-tools\_ask-project-directory.cmd YES .git %1
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 pushd %DEVF%\%PROJNAME%
 
-CALL %DEVT%\git-backup-remove.bat >NUL
+call %DEVT%\git-backup-remove.bat >NUL
 
 echo.
 echo Backup project (Just In Case)...
 echo.
 
-CALL %DEVT%\git-backup.bat >NUL
+call %DEVT%\git-backup.bat >NUL
 
 echo.
 echo Commiting project to subversion: %PROJNAME%
 echo.
 
-CALL %GIT% svn dcommit --rmdir
+call %GIT% svn dcommit --rmdir
 
 echo.
 CHOICE /C:YN /M "Was the commit successful"
 SET RESULT=%ERRORLEVEL%
 echo.
 
-IF %RESULT% EQU 1 CALL %DEVT%\git-backup-remove.bat
+IF %RESULT% EQU 1 call %DEVT%\git-backup-remove.bat
 
 popd
 
