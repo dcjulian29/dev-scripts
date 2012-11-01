d7be58b4260e57ef0908858f7ff3a84c37a2744e
Julian Easterling <julian@julianscorner.com>
2012-10-31 21:09:26 -0400

    Changed ArgoUML to keep user settings in the application folder instead of the user folder.

--- argouml.bat
+++ argouml.bat
@@ -1,4 +1,5 @@
 @echo off
-C:\bin\run-java start javaw -Xms64m -Xmx512m -cp argouml.jar org.argouml.application.Main %1 %2 %3 %4 %5 %6 %7 %8
+
+C:\bin\run-java start javaw -Duser.home=%CD% -Xms64m -Xmx512m -cp argouml.jar org.argouml.application.Main %1 %2 %3 %4 %5 %6 %7 %8
 
 if errorlevel 1 pause
