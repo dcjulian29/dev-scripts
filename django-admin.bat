47a31b436d0005a2273f6dcf82c2ff47fb662390
Julian Easterling <julian@julianscorner.com>
2013-02-28 20:27:04 -0500

    A little more intellegence in the scripts

--- django-admin.bat
+++ django-admin.bat
@@ -1,1 +1,2 @@
-@C:\python\python.exe C:\python\Lib\site-packages\django\bin\django-admin.py %*
+@echo off
+%SYSTEMDRIVE%\bin\python.bat C:\python\Lib\site-packages\django\bin\django-admin.py %*
--- django-manage.bat
+++ django-manage.bat
@@ -1,1 +1,15 @@
-C:\python\python.exe %CD%\manage.py %*
+@echo off
+
+if exist "%CD%\manage.py" goto :C1
+
+echo.
+echo This directory does not contain a Django project.
+echo.
+
+goto EOF
+
+:C1
+
+%SYSTEMDRIVE%\bin\python.bat %CD%\manage.py %*
+
+:EOF
