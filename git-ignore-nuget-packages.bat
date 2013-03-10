7c4e339e3389cdc6cc131775a9d0efb1e3b294ad
Julian Easterling <julian@julianscorner.com>
2013-03-10 16:16:41 -0400

    Some clients and projects want to use NuGet Package Restore and some don't so I updated my GIT Ignore file to not ignore the directory and created this script to add it to the project ignore file if it is decided to use NuGet Package Restore...

---
+++ git-ignore-nuget-packages.bat
@@ -0,0 +1,28 @@
+@echo off
+
+setlocal
+
+CALL C:\bin\development-tools\_dev_settings.cmd
+
+if exist "%CD%\.git" GOTO GITDIR
+
+echo.
+echo This directory does not contain a GIT repository
+echo.
+echo.
+GOTO EOF
+
+:GITDIR
+
+SET PROJNAME=
+FOR %%A in (%CD:\= %) DO SET PROJNAME=%%A
+
+SET SDIR=%DEVF%\%PROJNAME%
+
+echo.
+echo Adding an exclusion for NuGet Packages in this project...
+echo.
+
+call %DEVT%\git-add-ignore [Pp]ackages/
+
+:EOF
