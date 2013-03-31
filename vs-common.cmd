9dbaae538371a977f0bfb8658215884129d22322
Julian Easterling <julian@julianscorner.com>
2013-03-31 09:24:20 -0400

    Updated script to allow me to "list" solution files and the open them by number in the other Visual Studio scripts.

--- vs-common.cmd
+++ vs-common.cmd
@@ -1,4 +1,5 @@
 setlocal
+call C:\bin\development-tools\_dev_settings.cmd
 
 if [%1] NEQ [] goto C1
 
@@ -28,12 +29,49 @@ set PDIR="%ProgramFiles%\Microsoft Visual Studio %VSV%\Common7\IDE"
 
 :C2
 
+set SN=%2
+
+echo %SN%|findstr /r "[^0-9]" > nul
+if errorlevel 1 goto C2A
+
+goto C2B
+
+:C2A
+
+for /f "usebackq delims=" %%O in (`%DEVT%\vs-solutions.bat %SN%`) do SET SF=%%O
+
+if "%SF%" NEQ "" goto C2C
+
+echo.
+echo Visual Studio solution number %SN% not found...
+call %DEVT%\vs-solutions.bat
+goto EOF
+
+:C2C
+
+set PROJNAME="%SF%"
+
+goto C3
+
+:C2B
+
+if [%2] == [.] goto C3B
+
 set PROJNAME=%2
 
 if [%PROJNAME%] NEQ [] goto C3
 
-:: if project name/solution file not provided, try to find it.
+:: If nothing provided on command line, just start Visual Studio...
+if [%PROJNAME%] EQU [] goto STARTVS
+
+:C3B
+
+:: Attempt to determine the solution name if "." is provided.
+::  Will only match if SLN file is same name as folder...
 for %%A in (%CD:\= %) do set PROJNAME=%%A
+
+echo.
+echo Looking for a solution file named %PROJNAME%.sln
 
 :C3
 
@@ -42,8 +80,18 @@ if exist %PROJNAME% set SLN=%PROJNAME%
 
 if exist %SLN% goto STARTVS
 
+if [%PROJNAME%] NEQ [] goto NOTFOUND
+
 echo.
 echo Please provide the solution name.
+echo.
+
+goto EOF
+
+:NOTFOUND
+
+echo.
+echo That solution file does not exist.
 echo.
 
 goto EOF
--- vs-solutions.bat
+++ vs-solutions.bat
@@ -4,6 +4,8 @@ setlocal
 
 CALL C:\bin\development-tools\_dev_settings.cmd
 
+if [%1] NEQ [] SET LOOKINGFOR=%1
+
 set NUMSLN=0
 
 for /f "usebackq delims=" %%O in (`dir /B /S *.sln`) do call :LIST %%O
@@ -18,6 +20,7 @@ set SLNFILE=
 
 set IGNORE=0
 
+set SLN=%SLN: =/%
 FOR %%A in (%SLN:\= %) DO call :STRIP %%A
 
 if %IGNORE% == 0 goto SHOW
@@ -25,12 +28,27 @@ goto EOF
 
 :SHOW
 
+if [%LOOKINGFOR%] NEQ [] goto NOOUTPUT
+
 if %NUMSLN% == 0 echo.
+
+:NOOUTPUT
 
 set /A "NUMSLN=NUMSLN+1"
 
+set SLNFILE=%SLNFILE:/= %
+
+if [%LOOKINGFOR%] NEQ [] goto LOOKING
+
 if %NUMSLN% LSS 10 echo: %NUMSLN%: %SLNFILE%
 if %NUMSLN% GTR 9 echo %NUMSLN%: %SLNFILE%
+
+goto EOF
+
+:LOOKING
+
+if [%LOOKINGFOR%] == [%NUMSLN%] echo %SLNFILE%
+
 goto EOF
 
 :STRIP
