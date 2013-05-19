ac6bdda3857c9c579a0243cacaf18d881b17fd0d
Julian Easterling <julian@julianscorner.com>
2013-05-19 11:49:23 -0400

    script to update a workspace from a TFS server.

---
+++ tfs-update.bat
@@ -0,0 +1,33 @@
+@echo off
+
+setlocal
+
+call C:\bin\development-tools\_dev_settings.cmd
+call %DEVT%\_tfs_LoadSettings.cmd YES
+call %DEVT%\_tfs_Command.cmd
+
+if [%2] == [*] goto JUSTFILTER
+
+set PROJNAME=%1
+set FILTER=%2
+goto C5
+
+:JUSTFILTER
+
+set PROJNAME=
+set FILTER=%1
+
+:C5
+
+echo.
+echo.
+echo Updating %TFS-NS%/%PROJNAME%...
+echo.
+
+set TFCMD=%TFCMD% get
+set TFCMD=%TFCMD% "%TFS-NS%/%PROJNAME%"
+set TFCMD=%TFCMD% /recursive
+
+%TFCMD%
+
+:EOF
