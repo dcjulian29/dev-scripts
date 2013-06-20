ab6c6207e888b631cdc9e6e660b3fc27d676c47f
Julian Easterling <julian@julianscorner.com>
2013-06-19 22:03:11 -0400

    Removed script to enable Git Extensions since I'm not using it anymore.

--- git-extensions-enable.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Enabling Git Extensions...
-
-for /f "usebackq delims=" %%O in (`where dev-path.bat`) do set BINPATH=%%~dpO
-set DEVPATH=%BINPATH%development-tools
-
-path %DEVPATH%\GitExtensions;%PATH%
