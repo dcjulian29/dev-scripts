30b7c147fbac7b17b5f0c7189f08bb5424eb7b13
Julian Easterling <julian@julianscorner.com>
2013-07-27 16:17:10 -0400

    Added a script to pusblish a NuGet package to a NuGet server

---
+++ nuget-publish.bat
@@ -0,0 +1,36 @@
+@echo off
+
+setlocal
+
+call %SYSTEMDRIVE%\bin\development-tools\_dev_settings.cmd
+
+if [%1] NEQ [] goto PROCEED
+
+echo.
+echo Please provide the package name to publish
+echo.
+
+goto EOF
+
+:PROCEED
+
+if exist "%1" GOTO PUBLISH
+
+echo.
+echo The specified package does not exists.
+echo.
+
+goto EOF
+
+:PUBLISH
+
+call %DEVT%\NuGet\_loadSettings.cmd YES
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+set PATH=%DEVT%\NuGet;%PATH%
+
+%DEVT%\NuGet\nuget.exe push %1 %NUGET-API% -Source %NUGET-URL%
+
+:EOF
+
+endlocal
