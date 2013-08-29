5cefc3e292090166f00dbadf373d03e51bf9c1b4
Julian Easterling <julian@julianscorner.com>
2013-08-28 23:37:54 -0400

    Removed extra "per-machine" configurations and dropbox and custom "DEV" folders.

--- _dev_settings.cmd
+++ _dev_settings.cmd
@@ -1,72 +1,23 @@
-SET DEVF=C:\dev
-SET DEVR=C:\dev-retired
-SET DEVP=C:\dev-postponed
-
-:: My "Development Laptop" has a different configuration for some directories...
-IF [%COMPUTERNAME%] == [JULIAN] GOTO JULIAN
-
-:: Sometimes I work out of the Dropbox folder and need to the tools to look there...
-IF EXIST %TEMP%\dropbox-dev GOTO DROPBOX
-
-:: Sometimes I work directly in a custom folder and need the tools and scripts to use that folder.
-if exist %TEMP%\custom-dev GOTO CUSTOM
-
-:: Skip special environment configurations
-GOTO CONT
-
-:JULIAN
-
-:: On my laptop, I develop projects in multiple directories,
-::    so default to the parent of the current directory.
 pushd ..
 set DEVF=%CD%
 popd
+
 set DEVR=D:\_archives\dev
 set DEVP=%DEVF%\_postponed
 
-if exist %TEMP%\dropbox-dev goto DROPBOX
-if exist %TEMP%\custom-dev goto CUSTOM
-goto CONT
-
-:CUSTOM
-
-echo.
-echo Enabling Custom DEV configuration...
-echo.
-
-set DEVF=%CD%
-
-set /p ND="DEV directory [%DEVF%]? "
-if "" neq "%ND%" set DEVF=%ND%
-set DEVF=%DEVF: =%
-
-echo.
-
-goto CONT
-
-:DROPBOX
-
-echo.
-echo Enabling Dropbox DEV configuration...
-echo.
-SET DEVF=%USERPROFILE%\Dropbox
-GOTO CONT
-
-:CONT
-
-for /f "usebackq delims=" %%O in (`where _dev_settings.cmd`) do set DEVT=%%~dpO
+for /f "usebackq delims=" %%O in (`where %0`) do set DEVT=%%~dpO
 
 SET SVN_INSTALL_ROOT=%DEVT%\svn
 SET SVN=%SVN_INSTALL_ROOT%\bin\svn.exe
+PATH %SVN_INSTALL_ROOT%\bin;%PATH%
 
 SET GIT_INSTALL_ROOT=%DEVT%\msysgit
 SET GIT=%GIT_INSTALL_ROOT%\bin\git.exe
+PATH %GIT_INSTALL_ROOT%\bin;%PATH%
 
-::SET ZIP="C:\Program Files\7-Zip\7z.exe"
 set ZIP="C:\bin\File\7-Zip\App\7-Zip\7z.exe"
 SET NANT=%DEVT%\nant\bin\NAnt.exe
 SET NUNIT=%DEVT%\nunit\bin\nunit-console-x86.exe
 SET NSIS=%DEVT%\nsis\nsis.exe
 SET DOBUDISH=%DEVT%\DocBook\dobudish.cmd
 
-PATH %SVN_INSTALL_ROOT%\bin;%GIT_INSTALL_ROOT%\bin;%PATH%
