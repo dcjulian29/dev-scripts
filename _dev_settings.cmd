0a3fb7b88649fb9460a6b01c718704ee10d365a1
Julian Easterling <julian@julianscorner.com>
2013-09-30 16:16:34 -0400

    Updated DEVF so that I can weed out scripts that use that... Added finding MSBuild to script

--- _dev_settings.cmd
+++ _dev_settings.cmd
@@ -1,8 +1,10 @@
-pushd ..
-set DEVF=%CD%
-popd
-
-set DEVR=D:\_archives\dev
+::pushd ..
+::set DEVF=%CD%
+::popd
+:: Need to weed out any scripts that depend on a hard-coded "DEV" folder
+set DEVF=ZZZ:
+:: Need to weed out any scripts that use "postpone"... 
+::    just delete the folder and then check out from SCM later...
 set DEVP=%DEVF%\_postponed
 
 for /f "usebackq delims=" %%O in (`where %~n0`) do set DEVT=%%~dpO
@@ -21,3 +23,15 @@ SET NUNIT=%DEVT%\nunit\bin\nunit-console-x86.exe
 SET NSIS=%DEVT%\nsis\nsis.exe
 SET DOBUDISH=%DEVT%\DocBook\dobudish.cmd
 
+:: "Retirement" Directory where projects go to die...
+set DEVR=D:\_archives\dev
+
+
+:: Find MSBuild
+set MSBUILD=
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v*) do (
+  if exist %%D\MSBuild.exe set MSBUILD=%%D\MSBuild.exe
+)
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework64\v*) do (
+  if exist %%D\MSBuild.exe set MSBUILD=%%D\MSBuild.exe
+)
