9cc8bf5704b01790a9a68c91a4a20dc2481c42f9
Julian Easterling <julian@julianscorner.com>
2013-09-30 16:33:14 -0400

    Added script to output "my" TFS work items.

---
+++ tfs-workitems-mine.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+call %~dp0_dev_settings.cmd
+call %DEVT%\_tfs_LoadSettings.cmd YES
+
+set PSHELL=PowerShell -NoLogo -NonInteractive -Command
+
+pushd ..
+for /f "usebackq tokens=* delims=" %%U in (`my-username.bat`) do set FULLNAME=%%U
+popd
+
+echo.
+echo Work items for %FULLNAME% from %TFS-URL%/%TFS-COLLECTION%...
+echo.
+echo ID      State   Title
+echo ------------------------------------------------------------------------------------------------
+
+%PSHELL% "Get-TfsServer %TFS-URL% %TFS-COLLECTION% | Get-TfsWorkItem -User '%FULLNAME%'"
+
+echo.
+
+:EOF
