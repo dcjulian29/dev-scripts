50eb016e43e4276c943482a5bf8a14277edc2bde
Julian Easterling <julian@julianscorner.com>
2013-11-12 20:54:46 -0500

    updated to handle spaces in path and to display relative path of PDB files

--- remove-pdb-from-packages.bat
+++ remove-pdb-from-packages.bat
@@ -2,11 +2,33 @@
 
 if not exist "%CD%\Packages" goto EOF
 
+setlocal EnableDelayedExpansion
+
 pushd Packages
-FOR /F "tokens=*" %%G IN ('DIR /B /S *.pdb') DO (
-  echo %%G
-  rm -f %%G
+
+set ROOTPATH=%CD%
+set CUT=
+
+:LOOP
+if not "!ROOTPATH!" == "" (
+  set /a CUT += 1
+  set ROOTPATH=!ROOTPATH:~1!
+  goto LOOP
 )
+
+set /a CUT += 1
+
+for /F "tokens=*" %%G in ('dir /B /S *.pdb') do (
+  set FILENAME=%%~fG
+  set FILENAME=!FILENAME:~%CUT%!
+
+  echo == !FILENAME!
+
+  del /F /Q "%%G"
+)
+
 popd
+
+endlocal
 
 :EOF
