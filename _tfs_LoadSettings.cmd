c5aa0ce67c9816c3a8a2e4fdb766d7e269762397
Julian Easterling <julian@julianscorner.com>
2013-11-26 18:27:53 -0500

    updated to use current script path instead of environment variable for determination where to find "profile" scripts

--- _tfs_LoadSettings.cmd
+++ _tfs_LoadSettings.cmd
@@ -11,7 +11,7 @@ goto TFS-CHECK-PROFILE-EXISTS
 
 set TFS-PROFILE=
 
-call %DEVT%\_tfsprofile_template.cmd
+call %~dp0_tfsprofile_template.cmd
 
 :: If profile is not defined, check the first parameter to
 ::  see if we should prompt for the profile
@@ -27,7 +27,7 @@ set /p TPN="What is the name of the TFS profile to use? "
 
 set TPN=%TPN: =%
 
-if exist "%DEVT%\_tfsprofile_%TPN%.cmd" goto TFS-LOAD-PROFILE
+if exist "%~dp0_tfsprofile_%TPN%.cmd" goto TFS-LOAD-PROFILE
 
 echo.
 echo The TFS profile does not exist.
@@ -37,6 +37,6 @@ exit /b 1
 
 :TFS-LOAD-PROFILE
 
-call %DEVT%\_tfsprofile_%TPN%.cmd
+call %~dp0_tfsprofile_%TPN%.cmd
 
 if "" neq "%TPN%" set TFS-PROFILE=%TPN%
