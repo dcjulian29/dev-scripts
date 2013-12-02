8bfc00f0e8a5baf4cfc71a943b5c3ccd9dc3e7b4
Julian Easterling <julian@julianscorner.com>
2013-12-01 20:10:02 -0500

    added an alias script to pack chocolatey packages

---
+++ choco-make-package.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+call %~dp0_dev_settings.cmd
+
+set PKG=
+
+if [%1] EQU [] GOTO DEFAULTPACKAGE
+
+set PKG=%1
+
+goto PROCEED
+
+:DEFAULTPACKAGE
+
+set PKG=package.nuspec
+
+:PROCEED
+
+if exist "%PKG%" GOTO PACKAGE
+
+echo.
+echo The specified package specification does not exists.
+echo.
+
+goto EOF
+
+:PACKAGE
+
+chocolatey pack "%PKG% -Verbosity detailed"
+
+endlocal
