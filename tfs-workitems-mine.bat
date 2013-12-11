86ab3ac80cc11bd2cb575a297431c9390caf7539
Julian Easterling <julian@julianscorner.com>
2013-12-11 15:54:44 -0500

    switched script to use the "pshell.cmd" alias script

--- tfs-workitems-mine.bat
+++ tfs-workitems-mine.bat
@@ -1,9 +1,10 @@
-@echo off
+::@echo off
 setlocal
+
 call %~dp0_dev_settings.cmd
 call %DEVT%\_tfs_LoadSettings.cmd YES
 
-set PSHELL=PowerShell -NoLogo -NonInteractive -Command
+set PSHELL=call %SYSTEMDRIVE%\Tools\binaries\pshell.cmd 
 
 pushd ..
 for /f "usebackq tokens=* delims=" %%U in (`my-username.bat`) do set FULLNAME=%%U
@@ -13,8 +14,10 @@ echo.
 echo Work items for %FULLNAME% from %TFS-URL%/%TFS-COLLECTION%...
 echo.
 
-%PSHELL% "Get-TfsServer %TFS-URL% %TFS-COLLECTION% | Get-TfsWorkItem -User '%FULLNAME%'"
+%PSHELL% Get-TfsWorkItem -TfsServer (Get-TfsServer %TFS-URL% %TFS-COLLECTION%) -User '%FULLNAME%'
 
 echo.
+
+endlocal
 
 :EOF
