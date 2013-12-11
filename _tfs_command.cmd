2954ea079a0a1b0b57e90e17a90b12b60d95b996
Julian Easterling <julian@julianscorner.com>
2013-12-11 15:45:41 -0500

    Added TF tool from Visual Studio 2013

--- _tfs_command.cmd
+++ _tfs_command.cmd
@@ -15,12 +15,18 @@ set PDIR="%ProgramFiles%"
 set TFCMD=
 set PDIR=%PDIR:"=%
 
+if exist "%PDIR%\Microsoft Visual Studio 12.0\Common7\IDE\TF.exe" goto TF12
 if exist "%PDIR%\Microsoft Visual Studio 11.0\Common7\IDE\TF.exe" goto TF11
 if exist "%PDIR%\Microsoft Visual Studio 10.0\Common7\IDE\TF.exe" goto TF10
 
 echo Could not find TFS program. Is it installed?
 echo.
 exit /b 1
+
+:TF12
+
+set TFCMD="%PDIR%\Microsoft Visual Studio 12.0\Common7\IDE\TF.exe"
+goto EOF
 
 :TF11
 
