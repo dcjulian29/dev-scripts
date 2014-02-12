cd9b9453a95c92bab96617753b066c07274110f0
Julian Easterling <julian@julianscorner.com>
2014-02-11 22:35:17 -0500

    Added TFS 2013 cache directory

--- tfs-clear-cache-local.bat
+++ tfs-clear-cache-local.bat
@@ -12,12 +12,18 @@ set TFAPP=%USERPROFILE%\AppData\Local\Microsoft\Team Foundation
 call :REMOVECACHE 2.0
 call :REMOVECACHE 3.0
 call :REMOVECACHE 4.0
+call :REMOVECACHE 5.0
+
+endlocal
 
 goto EOF
 
 :REMOVECACHE
 
-%SYSTEMDRIVE%\bin\dir-rm.bat %TFAPP%\%1\Cache
-mkdir %TFAPP%\%1\Cache
+
+if not exist "%TFAPP%\%1\Cache" goto EOF
+
+call %SYSTEMDRIVE%\tools\binaries\dir-rm.bat "%TFAPP%\%1\Cache"
+mkdir "%TFAPP%\%1\Cache"
 
 :EOF
