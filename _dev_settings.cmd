9a173d2cd97b1fe6a91530de27a4f36eae9e42a9
Julian Easterling <julian@julianscorner.com>
2014-02-11 22:43:08 -0500

    Updated script to only load once and to determine where the 32-bit program files directory is. Also updated to add variables for the "tools" directory.

--- _dev_settings.cmd
+++ _dev_settings.cmd
@@ -1,3 +1,5 @@
+if [%DEVSETTINGS%] equ [LOADED] goto EOF
+
 ::pushd ..
 ::set DEVF=%CD%
 ::popd
@@ -6,6 +8,8 @@ set DEVF=ZZZ:
 :: Need to weed out any scripts that use "postpone"... 
 ::    just delete the folder and then check out from SCM later...
 set DEVP=%DEVF%\_postponed
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 for /f "usebackq delims=" %%O in (`where %~n0`) do set DEVT=%%~dpO
 
@@ -20,15 +24,26 @@ set PDIR="%ProgramFiles%"
 
 :CONT
 
+set PDIR=%PDIR:"=%
+
+if not exist "%PDIR%\Git" goto NOGIT
+
 set GIT_INSTALL_ROOT=%PDIR%\Git
 set GIT="%GIT_INSTALL_ROOT%\bin\git.exe"
 path "%GIT_INSTALL_ROOT%\bin";%PATH%
 
-set ZIP="%SYSTEMDRIVE%\Tools\binaries\7Zip.cmd"
+:NOGIT
+
+set TOOLDIR=%SYSTEMDRIVE%\Tools
+set TOOLAPP=%TOOLDIR%\apps
+set TOOLBIN=%TOOLDIR%\binaries
+set TOOLDEV=%TOOLDIR%\development
+
+set ZIP="%TOOLBIN%\7Zip.cmd"
 set NANT=%DEVT%\nant\bin\NAnt.exe
 set NUNIT=%DEVT%\nunit\bin\nunit-console-x86.exe
 set NSIS=%DEVT%\nsis\nsis.exe
-set DOBUDISH=%DEVT%\DocBook\dobudish.cmd
+set DOBUDISH=%TOOLDEV%\DocBook\dobudish.cmd
 
 :: Find MSBuild
 set MSBUILD=
@@ -39,9 +54,13 @@ for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework64\v*) do (
   if exist %%D\MSBuild.exe set MSBUILD=%%D\MSBuild.exe
 )
 
-:: As of 4.5.1 (VS 2013), MSBuild has been removed from the "framework" and added to its own MSBuild tool
+:: As of 4.5.1 (VS 2013), MSBuild has been removed from the "framework"
 for /D %%D in ("C:\Program Files (x86)\MSBuild\*") do (
   if exist %%D\bin\MSBuild.exe set MSBUILD="%%D\bin\MSBuild.exe"
 )
 
 set PSHELL=call %SYSTEMDRIVE%\Tools\binaries\pshell.cmd 
+
+set DEVSETTINGS=LOADED
+
+:EOF
