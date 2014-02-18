a0a8ded355cbebcae544eca7c6b32eacfa021691
Julian Easterling <julian@julianscorner.com>
2014-02-18 18:18:07 -0500

    updated script to find GIT root directory and to remove quotes around GIT variable.

--- git-info.bat
+++ git-info.bat
@@ -8,23 +8,33 @@ call %~dp0_dev_settings.cmd
 pushd
 
 :: Find base of git directory
+
+:LOOP
+
 ::while [ ! -d .git ] && [ ! `pwd` = "/" ]; do cd ..; done
+if %CD:~0,3% EQU %CD% goto CONT
+dir /b | findstr /L /C:".git" >nul
+if %ERRORLEVEL% EQU 0 goto CONT
+cd ..
+goto LOOP
+
+:CONT
 
 IF EXIST %CD%\.git (
   ECHO == Remote URLs:
-  "%GIT%" remote -v
+  %GIT% remote -v
   ECHO.
 
   ECHO == Branches:
-  "%GIT%" branch -a
+  %GIT% branch -a
   ECHO.
 
   ECHO == Configuration:
-  "%GIT%" config --local --list
+  %GIT% config --local --list
   ECHO.
 
   echo == Most Recent Commit
-  "%GIT%" --no-pager log --max-count=1
+  %GIT% --no-pager log --max-count=1
   ECHO.
 
   ECHO Type 'git log' for more commits, or 'git show' for full commit details.
@@ -33,3 +43,5 @@ IF EXIST %CD%\.git (
 )
 
 popd
+
+endlocal
