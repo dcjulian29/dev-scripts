ce801a3649cdc6da308b63568d40357f1179ad01
Julian Easterling <julian@julianscorner.com>
2014-04-19 22:08:48 -0400

    updated script to move most of the logic into the XXE script...

--- docbook-edit.bat
+++ docbook-edit.bat
@@ -1,28 +1,14 @@
-@echo off
+::@echo off
 setlocal
 call %~dp0_dev_settings.cmd
 
-call %DEVT%\DocBook\docbook-ask-project.cmd YES %1
+call %TOOLDEV%\DocBook\docbook-ask-project.cmd YES %1
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 echo.
 echo Editing DocBook project...
 echo.
 
-set ODIR=%CD%
-
-pushd %SystemDrive%\bin\development-tools\DocBook\dobudish
-
-rmdir /s /q documents 2> null
-
-mkdir documents
-
-call %SystemDrive%\bin\junction.bat documents\%DOCNAME% %ODIR%\%DOCNAME% > nul
-
-CALL %DEVT%\xxe.cmd -read %CD%\documents\%DOCNAME%\input\%DOCNAME%.xml
-
-::rmdir /s /q documents
-
-popd
+call %TOOLDEV%\DocBook\xxe.cmd %DOCNAME%
 
 endlocal
