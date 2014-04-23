cd6a26a1d0fef971138d1468720bb8ece35afd4b
Julian Easterling <julian@julianscorner.com>
2014-04-22 21:49:10 -0400

    combined the various TFS workspace scripts into one that spits out the "collection" from the "common" part of the TFS profiles. Will need to update script to ask for a profile if the script does not find the "common" part.

--- tfs-workspace.bat
+++ tfs-workspace.bat
@@ -1,8 +1,7 @@
 @echo off
 setlocal
 call %~dp0_dev_settings.cmd
-
-call %DEVT%\_tfs_LoadSettings.cmd YES
+call %SYSTEMDRIVE%\etc\tfs\_common.bat
 call %DEVT%\_tfs_Command.cmd
 
 set TFCMD=%TFCMD% workspaces
