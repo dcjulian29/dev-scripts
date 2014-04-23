7b3b4278aed0de79cd2d71bf5a7d78b705560506
Julian Easterling <julian@julianscorner.com>
2014-04-22 22:06:20 -0400

    updated to work on systems that use multiple TFS collections so that system will not have a "common" file with the collection name.

--- tfs-workspace.bat
+++ tfs-workspace.bat
@@ -1,8 +1,20 @@
 @echo off
 setlocal
 call %~dp0_dev_settings.cmd
+
+if not exist %SYSTEMDRIVE%\etc\tfs\_common.bat goto LOADPROFILE
+
 call %SYSTEMDRIVE%\etc\tfs\_common.bat
-call %DEVT%\_tfs_Command.cmd
+
+goto EXECUTE
+
+:LOADPROFILE
+
+call %TOOLDEV%\_tfs_LoadSettings.cmd YES
+
+:EXECUTE
+
+call %TOOLDEV%\_tfs_Command.cmd
 
 set TFCMD=%TFCMD% workspaces
 set TFCMD=%TFCMD% /collection:"%TFS-URL%/%TFS-COLLECTION%" /format:detailed
