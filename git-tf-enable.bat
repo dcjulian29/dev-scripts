919d36781365ceb06bfa59ea0cdb715290b1e6d1
Julian Easterling <julian@julianscorner.com>
2015-02-23 15:37:40 -0500

    I could never get the Git-TFS and Git-TF projects to work with the TFS projects I had to work with so I've reverted to just managing them with TFS tools (or VS) and to convert to or use Git repositories whenever I can...

--- git-tf-enable.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Enabling git-tf...
-
-for /f "usebackq delims=" %%O in (`where dev-path.bat`) do set BINPATH=%%~dpO
-set DEVPATH=%BINPATH%development-tools
-
-path %DEVPATH%\git-tf;%PATH%
