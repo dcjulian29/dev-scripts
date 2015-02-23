564a8acbde17ba7a7331474869ce3c9ad1bf16b3
Julian Easterling <julian@julianscorner.com>
2015-02-23 15:09:16 -0500

    Updated the "Update Watched Projects" to also work with Git repositories.

--- update-watched-projects.bat
+++ update-watched-projects.bat
@@ -3,13 +3,23 @@ setlocal
 call %~dp0_dev_settings.cmd
 
 ECHO.
-ECHO Searching for SVN projects to update...
+ECHO Updating the watched projects in this directory...
 ECHO.
 
 FOR /d %%D in (%CD%\*) DO (
   IF NOT EXIST %%D\.svn (
-    :: This folder is a non SCM folder
-    ECHO -- %%D
+    IF NOT EXIST %%D\.git (
+      :: This folder is a non SCM folder
+      ECHO -- %%D
+    ) ELSE (
+      :: This folder is a GIT folder
+      ECHO == %%D
+      echo.
+      pushd %%D
+      %GIT% pull --all 
+      %GIT% gc
+      popd
+    )
   ) ELSE (
     :: This folder is a SVN folder
     ECHO ++ %%D
